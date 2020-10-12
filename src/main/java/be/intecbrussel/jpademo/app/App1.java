cf8bd9810951ef9f198048f7598b7752c66b9958
aOUINA1994 <aouina_@hotmail.com>
2020-10-12 13:40:26 +0200

    META INF

---
+++ src/main/java/be/intecbrussel/jpademo/app/App1.java
@@ -0,0 +1,45 @@
+package be.intecbrussel.jpademo.app;
+
+import be.intecbrussel.jpademo.model.Person;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class App1 {
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        person.setFirstName("Super");
+        person.setLastname("Man");
+        person.setAge(30);
+        person.setAddress("America");
+        person.setId(1);
+
+        EntityManagerFactory emf = null;
+        EntityManager entityManager = null;
+
+
+        try{
+            emf = Persistence.createEntityManagerFactory("mypersistenceunit");
+            entityManager = emf.createEntityManager();
+            EntityTransaction transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(person);
+            transaction.commit();
+            System.out.println("person saved");
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+        finally{
+            if(entityManager != null){
+                entityManager.close();
+            }
+            if(emf != null){
+                emf.close();
+            }
+        }
+    }
+}
